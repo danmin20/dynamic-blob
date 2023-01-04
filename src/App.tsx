9a6531a80daa68eede59db109815ad8c545fc50d
lee jeongmin <ljm991108@gmail.com>
2023-01-04 22:45:43 +0900

    feat: dynamic blob 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,94 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import "./App.scss";
+import { useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
+  let selected: HTMLDivElement | null = null;
+  const handleMousedown = (dom: HTMLDivElement) => {
+    console.log("handleMousedown");
+    selected = dom;
+  };
+
+  useEffect(() => {
+    const doms = document.getElementById("dynamic-blob")?.childNodes ?? [];
+
+    doms.forEach((dom) => {
+      dom.addEventListener("mousedown", () =>
+        handleMousedown(dom as HTMLDivElement)
+      );
+    });
+
+    return () => {
+      const doms = document.getElementById("dynamic-blob")?.childNodes ?? [];
+      doms.forEach((dom) => {
+        dom.removeEventListener("mousedown", () =>
+          handleMousedown(dom as HTMLDivElement)
+        );
+      });
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleMouseup = () => {
+      if (!selected) return;
+      console.log(selected, "handleMouseup");
+      selected.style.animation = "bounceBack 1s ease";
+      selected.style.animationFillMode = "forwards";
+      selected = null;
+    };
+
+    const handleMousemove = (e: MouseEvent) => {
+      if (!selected) return;
+      console.log(selected, "handleMousemove");
+      selected.style.left =
+        e.clientX -
+        selected.getBoundingClientRect().width / 2 -
+        window.innerWidth / 2 +
+        "px";
+      selected.style.top =
+        e.clientY -
+        selected.getBoundingClientRect().height / 2 -
+        window.innerHeight / 2 +
+        "px";
+      selected.style.animation = "";
+    };
+
+    window.addEventListener("mouseup", () => handleMouseup());
+    window.addEventListener("mousemove", (e) => handleMousemove(e));
+
+    return () => {
+      window.removeEventListener("mouseup", () => handleMouseup());
+      window.removeEventListener("mousemove", (e) => handleMousemove(e));
+    };
+  }, [selected]);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="dynamic-blob" className="dynamic-blob">
+        {[...new Array(15)].map((_, idx) => (
+          <div id={idx.toString()} key={idx} />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+        <defs>
+          <filter id="goo">
+            <feGaussianBlur
+              in="SourceGraphic"
+              stdDeviation="10"
+              result="blur"
+            />
+            <feColorMatrix
+              in="blur"
+              mode="matrix"
+              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
+              result="goo"
+            />
+            <feComposite in="SourceGraphic" in2="goo" operator="atop" />
+          </filter>
+        </defs>
+      </svg>
     </div>
   );
-}
+};
 
 export default App;
